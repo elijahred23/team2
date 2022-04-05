e6a73907567772e5f99e6ba1469d5133f40b6802
Di-Chi <93900629+Di-Chi@users.noreply.github.com>
2022-04-05 15:57:11 -0500

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 if(isset($_POST['submit'])){
-    $to = "ask@cameron.libanswers.com"; // this is your Email address
+    $to = "rn904391@cameron.edu"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
